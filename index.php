bfa8282dfc5cb8e04529c4980ac41d543de61b35
zealx2 <114329873+zealx2@users.noreply.github.com>
2022-10-14 22:20:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For Post</title>
+</head>
+<body>
+    <form method="POST" action="user_data.php">
+        <label for="name">
+            Name: <input type="text" name="name">
+         </label><br>
+            <label for="email">
+         Email: <input type="text" name="email">
+            </label><br>
+                <label for="dob">
+            Date of Birth: <input type="text" name="dob">
+            <label for="gender"></label><br>
+            Gender: <input type="text" name="gender">
+            <label for="country">
+            Country: <input type="text" name="country"></label><br>
+
+         <button type="submit">Submit</button>
+     </form>
+</body>
+</html>
---
+++ user_data.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+$country = $_POST['country'];
+
+echo "NAME: " . $name . "<br>";
+echo "EMAIL: " . $email . "<br>";
+echo "DATE OF BIRTH: " . $dob . "<br>";
+echo "GENDER: " . $gender . "<br>";
+echo "COUNTRY: " . $country . "<br>";
+?>
